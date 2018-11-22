1871a70afdd8527e0c4411d9ec0c66744ab5f6dc
adamtisza <tisza.adam.1@gmail.com>
2018-11-22 19:34:28 +0100

    Fixed problems with mergeSort algorithm, refreshed diagrams to represent new values.

--- TL_Feladat04/src/program.java
+++ TL_Feladat04/src/program.java
@@ -1,8 +1,5 @@
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ExecutorService;
-import java.lang.Runnable;
+import java.util.concurrent.*;
 
 public class program {
 	public static void main(String[] args) {
@@ -11,7 +8,7 @@ public class program {
         int[] block = generateBlock(size); 
         long lStartTime = 0;
         long lEndTime = 0;
-        int minBlockSize = 100;
+        final int minBlockSize = 1000;
         
         
         switch (args[1])
@@ -21,8 +18,9 @@ public class program {
                 quickSort(block, 0, size - 1); 
                 break;
             case "2":
+            	ExecutorService es = Executors.newCachedThreadPool();
             	lStartTime = System.nanoTime();
-                mergeSortInPlace(block, 0, size - 1, minBlockSize);
+                mergeSort(block, 0, size - 1, minBlockSize, es);
                 break;
             default:
                 break;
@@ -32,141 +30,134 @@ public class program {
         
         System.out.println(elapsed + " us");
         
-
-        
-		
 	}
+
+	static int[] generateBlock(int size) {
+		int[] block = new int[size];
+		Random rnd = new Random();
+
+		for (int i = 0; i < size; i++) {
+			block[i] = rnd.nextInt();
+		}
+		return block;
+
+	}
+
+	static int partition(int[] arr, int left, int right) {
+		int i = left, j = right;
+		int tmp;
+		int pivot = arr[(left + right) / 2];
+
+		while (i <= j) {
+
+			while (arr[i] < pivot)
+				i++;
+			while (arr[j] > pivot)
+				j--;
+			if (i <= j) {
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+				i++;
+				j--;
+			}
+		}
+		;
+		return i;
+	}
+
+	static void quickSort(int[] arr, int left, int right) {
+		int index = partition(arr, left, right);
+		if (left < index - 1)
+			quickSort(arr, left, index - 1);
+		if (index < right)
+			quickSort(arr, index, right);
+	}
+
+	static void mergeSort(int[] array, int left, int right, int minArraySize, ExecutorService es) {
+		if (right <= left)
+			return;
+		int mid = ((right + left) / 2);
+		if (right - mid >= minArraySize) {
+			Runnable task1 = () -> {
+				mergeSort(array, left, mid, minArraySize, es);
+			};
+			Runnable task2 = () -> {
+				mergeSort(array, mid + 1, right, minArraySize, es);
+			};
+
+			try {
+				Future f1 = es.submit(task1);
+				Future f2 = es.submit(task2);
+				f1.get();
+				f2.get();
+			} catch (ExecutionException ee) {
+				System.out.println("Execution failed!");
+			} catch (InterruptedException ie) {
+				System.out.println("Task interrupted!");
+			}
+		} else {
+			mergeSort(array, left, mid, minArraySize, es);
+			mergeSort(array, mid + 1, right, minArraySize, es);
+		}
+		merge(array, left, mid, right);
+	}
+
 	
-	static int[] generateBlock(int size)
-    {
-        int[] block = new int[size];
-        Random rnd = new Random();
 
-        for(int i = 0; i < size; i++)
-        {
-            block[i] = rnd.nextInt();
-        }
-        return block;
+	static void merge(int arr[], int l, int m, int r) 
+	{ 
+	    int i, j, k; 
+	    int n1 = m - l + 1; 
+	    int n2 =  r - m; 
+	  
 
-    }
+	    int[] L = new int[n1];
+	    int[] R = new int[n2];
+	  
+
+	    for (i = 0; i < n1; i++) 
+	        L[i] = arr[l + i]; 
+	    for (j = 0; j < n2; j++) 
+	        R[j] = arr[m + 1+ j]; 
+	  
+	    i = 0;
+	    j = 0;
+	    k = l; 
+	    while (i < n1 && j < n2) 
+	    { 
+	        if (L[i] <= R[j]) 
+	        { 
+	            arr[k] = L[i]; 
+	            i++; 
+	        } 
+	        else
+	        { 
+	            arr[k] = R[j]; 
+	            j++; 
+	        } 
+	        k++; 
+	    } 
+	  
+
+	    while (i < n1) 
+	    { 
+	        arr[k] = L[i]; 
+	        i++; 
+	        k++; 
+	    } 
+	  
+
+	    while (j < n2) 
+	    { 
+	        arr[k] = R[j]; 
+	        j++; 
+	        k++; 
+	    } 
+	} 
+	  
 	
-	static int partition(int[] arr, int left, int right)
-    {
-        int i = left, j = right;
-        int tmp;
-        int pivot = arr[(left + right) / 2];
+	
+	
 
-        while (i <= j)
-        {
-
-            while (arr[i] < pivot)
-                i++;
-            while (arr[j] > pivot)
-                j--;
-            if (i <= j)
-            {
-                tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-                i++;
-                j--;
-            }
-        };
-        return i;
-    }
-
-    static void quickSort(int[] arr, int left, int right)
-    {
-        int index = partition(arr, left, right);
-        if (left < index - 1)
-            quickSort(arr, left, index - 1);
-        if (index < right)
-            quickSort(arr, index, right);
-    }
-    
-    static void mergeSortInPlace( int[] array, int left, int right , int minArraySize)
-    {
-        if ( right <= left ) return;
-        int mid = (( right + left ) / 2 );
-        if(right-mid >= minArraySize) {
-        	ExecutorService es = Executors.newFixedThreadPool(2);
-        	
-        	Runnable task1 = () -> {
-        		mergeSortInPlace( array, left,     mid, minArraySize);
-        	};
-        	Runnable task2 = () -> {
-        		mergeSortInPlace( array, mid + 1, right, minArraySize);
-        	};
-        	
-
-        		es.execute(task1);
-        		es.execute(task2);
-
-        	
-        	
-        	es.shutdown();
-        	try {
-    			es.awaitTermination(10, TimeUnit.SECONDS);
-    		} catch (InterruptedException e) {
-    			
-    		}
-        }
-        else {
-        	mergeSortInPlace( array, left,     mid, minArraySize);
-        	mergeSortInPlace( array, mid + 1, right, minArraySize);
-        }
-        inPlaceMerge( array, left, mid, right );
-    }
-    
-    private static void inPlaceMerge(int[] array, int left, int mid, int right) {
-        int secondArrayStart = mid+1;
-        int prevPlaced = mid+1;
-        int q = mid+1;
-        while(left < mid+1 && q <= right){
-            boolean swapped = false;
-            if(array[left] > array[q]) {
-                swap(array, left, q);
-                swapped = true;
-            }   
-            if(q != secondArrayStart && array[left] > array[secondArrayStart]) {
-                swap(array, left, secondArrayStart);
-                swapped = true;
-            }
-            
-            if(swapped && secondArrayStart+1 <= right && array[secondArrayStart+1] < array[secondArrayStart]) {
-                prevPlaced = placeInOrder(array, secondArrayStart, prevPlaced);
-            }
-            left++;
-            if(q < right) {     //q+1 <= r) {
-                q++;
-            }
-        }
-    }
-    
-    static int placeInOrder(int[] array, int secondArrayStart, int prevPlaced) {
-        int i = secondArrayStart;
-        for(; i < array.length; i++) {
-            //Simply swap till the prevPlaced position
-            if(secondArrayStart < prevPlaced) {
-                swap(array, secondArrayStart, secondArrayStart+1);
-                secondArrayStart++;
-                continue;
-            }
-            if(array[i] < array[secondArrayStart]) {
-                swap(array, i, secondArrayStart);
-                secondArrayStart++;
-            } else if(i != secondArrayStart && array[i] > array[secondArrayStart]){
-                break;
-            }
-        }
-        return secondArrayStart;
-    }
-
-    private static void swap(int[] array, int m, int n){
-        int temp = array[m];
-        array[m] = array[n];
-        array[n] = temp;
-    }
-    
-    
 }
